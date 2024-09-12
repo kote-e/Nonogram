d691a9601b340207af3cf07a57084486b046ded2
vq0001 <vquiroga2023@udec.cl>
2024-09-12 18:56:59 -0300

    Crear clase enum Etapa

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pygame, sys
 from constantes import *
+from enum import Enum
+
+class Etapa(Enum):
+    INICIO = 1
+    NIVELES = 2
+    TABLERO = 3
 
 
 def main():
@@ -11,12 +17,20 @@ def main():
     screen.fill(BACKGROUND_COLOR)
     
     jugando = True
+    etapaJuego = Etapa.TABLERO
+
 
     while jugando:
         clock.tick(60)
         # Manejar eventos en el juego en cada funcion por separado
+
+        if etapaJuego == Etapa.TABLERO:
+            pass
+
         pygame.display.flip()
         
+
+
     
 
 main()

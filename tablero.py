1c056a3117934d541bf3b58d30a28f8020cee4a3
vq0001 <vquiroga2023@udec.cl>
2024-09-23 21:32:32 -0300

    Transformar la funcion drawGrid a una clase en un archivo aparte

--- tablero.py
+++ tablero.py
@@ -2,77 +2,8 @@
 import pygame, sys
 from constantes import *
 from BotonBloque import BotonBloque
-        
+from Grid import Grid
 
-def drawGrid(screen, blockCant, matriz):
-    
-    grillaSize = 410
-    grilla = pygame.Surface((grillaSize, grillaSize))
-    grilla.fill(GREEN)
-
-
-    margin = 2
-
-    blockSize = int((grillaSize - margin*blockCant)/ blockCant  - 1) 
-    step = blockSize + margin
-    
-
-    for x in range(0, blockCant):
-        for y in range(0,blockCant):
-
-            posX = x + margin + step*x
-            posY = y + margin + step*y
-
-            rect = pygame.Rect(posX, posY, blockSize, blockSize)
-            button = blockButton(rect, x, y, matriz)
-
-            button.draw(grilla, 1)
-            
-    
-    screen.blit(grilla, (390, 110))
-    
-
-
-def drawNumberIndicators(screen, blockCant, matriz):
-
-    superficieColumnas = pygame.Surface((410, 95))
-    superficieFilas = pygame.Surface((115, 410))
-
-    superficieColumnas.fill(GREEN)
-    superficieFilas.fill(GREEN)
-
-    margin = 2
-    anchoColumna = (410 - margin*blockCant)/ blockCant  - 1
-    anchoFila = (410 - margin*blockCant)/ blockCant  - 1
-
-    
-    
-    for x in range(blockCant): # dibujar numeros
-        for y in range(blockCant):
-            posC = x + margin + (anchoColumna + margin)*x
-            posF = y + margin + (anchoFila + margin)*y
-            
-            # dibujar columnas
-            rect = pygame.Rect(posC, 0, anchoColumna, 95)
-            pygame.draw.rect(superficieColumnas, SALMON, rect)
-
-            font = pygame.font.Font(None, 36)
-            text = font.render(str(x), True, DARK_BLUE)
-            superficieColumnas.blit(text, (x*40, 0))
-
-
-            # dibujar filas
-            rect = pygame.Rect(0, posF, 115, anchoFila)
-            pygame.draw.rect(superficieFilas, YELLOW, rect)
-
-            # font = pygame.font.Font(None, 36)
-            # text = font.render(str(x), True, DARK_BLUE)
-            # superficieColumnas.blit(text, (x*40, 0))
-
-
-    screen.blit(superficieColumnas, (390, 10))
-    screen.blit(superficieFilas, (270, 110))
-    
 
 def manejarEventos(self, blockSize, matrizValoresBloques):
         for event in pygame.event.get():

b42794c77d445b6d1010a93e9f7922791464de5d
Arcisternas <arcisternas2023@udec.cl>
2024-10-08 16:52:17 -0300

    fix: Mover lógica de verificación
    -Las funciónes para verificar y tachar el tablero, se mueven de la clase "Tablero" a la clase "Grid"

--- Grid.py
+++ Grid.py
@@ -8,7 +8,7 @@ class Grid():
     initialized = False # variables estaticas para inicializar la matriz una sola vez por tablero
     # matriz de botones
 
-    def __init__(self, blockCant, matrizValoresBloques, matrizIndices):
+    def __init__(self, blockCant, matrizValoresBloques, matrizIndices, matrizSolucion):
         self.blockCant = blockCant
         self.blockSize = 0
         self.grillaSize = 410
@@ -16,6 +16,7 @@ class Grid():
         self.matrizBloques = self.inicializarMatrizBloques(matrizValoresBloques)
         self.matrizIndices = matrizIndices
         self.matrizValoresBloques = matrizValoresBloques
+        self.matrizSolucion = matrizSolucion
        # self.value = 1 # 1 marcado, 2 cruz
 
     def inicializarMatrizBloques(self, matrizValoresBloques):
@@ -101,6 +102,61 @@ class Grid():
         screen.blit(superficieImagenBorde, (30, 195))
         screen.blit(superficieImagen, (35, 200))
 
+    def comprobarTablero(self):
+        # Cuando se marca un cuadro, las columnas y filas estan invertidas en matrizValoresBloques, por lo que transponemos la matriz
+        matrizTranspuesta = [[0 for i in range(self.blockCant)] for j in range(self.blockCant)]
+        for i in range(self.blockCant):
+            for j in range(self.blockCant):
+                matrizTranspuesta[j][i] = self.matrizValoresBloques[i][j]
+        """
+        Para ver la matriz impresa
+        for i in range(self.blockCant):
+            print(self.matrizValoresBloques[i])
+        for i in range(self.blockCant):
+            print(matrizTranspuesta[i])
+        """
+        if matrizTranspuesta == self.matrizSolucion:
+            print("Tablero resuelto")
+            
+    def comprobarFila(self, numFila): # numFila es un entero que indica la fila de matrizValoresBloques a comprobar
+        matrizTranspuesta = [[0 for i in range(self.blockCant)] for j in range(self.blockCant)] #Por el mismo motivo que en comprobarTablero
+        for i in range(self.blockCant):
+            for j in range(self.blockCant):
+                matrizTranspuesta[j][i] = self.matrizValoresBloques[i][j]
+                if matrizTranspuesta[j][i] == 2: #Para evitar errores, matrizTranspuesta solo tendra 0 y 1
+                    matrizTranspuesta[j][i] = 0
+        if matrizTranspuesta[numFila] == self.matrizSolucion[numFila]:
+            return True
+        else:
+            return False
+        
+    def comprobarColumna(self, numColumna): # numColumna es un entero que indica la columna de matrizValoresBloques a comprobar
+        matrizTranspuesta = [[0 for i in range(self.blockCant)] for j in range(self.blockCant)] #Por el mismo motivo que en comprobarTablero
+        for i in range(self.blockCant):
+            for j in range(self.blockCant):
+                matrizTranspuesta[j][i] = self.matrizValoresBloques[i][j]
+                if matrizTranspuesta[j][i] == 2: #Para evitar errores, matrizTranspuesta solo tendra 0 y 1
+                    matrizTranspuesta[j][i] = 0
+        if [matrizTranspuesta[i][numColumna] for i in range(self.blockCant)] == [self.matrizSolucion[i][numColumna] for i in range(self.blockCant)]:
+            return True
+        else:
+            return False
+        
+    def tacharFila(self, numFila):
+        for i in range(self.blockCant):
+            if self.matrizValoresBloques[i][numFila] == 0:
+                self.matrizValoresBloques[i][numFila] = 2
+
+    def tacharColumna(self, numColumna):
+        for i in range(self.blockCant):
+            if self.matrizValoresBloques[numColumna][i] == 0:
+                self.matrizValoresBloques[numColumna][i] = 2
+
+    def comprobarTachar(self, numFila, numColumna):
+        if self.comprobarFila(numFila):
+            self.tacharFila(numFila)
+        if self.comprobarColumna(numColumna):
+            self.tacharColumna(numColumna)
 
     def getBlockSize(self):
         return self.blockSize   
